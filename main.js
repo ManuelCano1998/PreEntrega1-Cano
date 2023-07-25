76baf77dcec27e06f6dc5fdf9de33bef150a0a53
Manuel Cano <manuelcano406@gmail.com>
2023-07-24 22:11:36 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+// let nombre = prompt("Ingrese su nombre y apellido");
+// alert("Hola " + nombre);
+
+// let numero = prompt("Ingrese su altura");
+// alert("Su altura es: " + numero);
+
+
+let edad = parseFloat(prompt("Ingresar su edad"));
+
+ if (edad == 18) {
+     alert("Puedes inscribirte al curso")
+ }
+
+ if (edad < 17) {
+     alert("No puedes inscribirte al curso")
+ }
+
+
+//  let entrada = prompt("Ingresar un nombre");
+
+//  while (entrada != "ESC") {
+//    switch (entrada) {
+//        case "Camila":
+//        alert("Si! Adivinaste! Hola Camila");
+//       break;
+//       case "Manuel":
+//        alert("Si! Adivinaste! Hola Manuel");
+//        break;
+//      default:
+//        alert("¿Cual es el nombre correcto?");
+//        break;
+//    }
+//    entrada = prompt("Ingresar un nombre");
+// }
