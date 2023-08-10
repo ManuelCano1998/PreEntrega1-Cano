e991060c73d8394d8754d556a8b3b57cf08b1b40
Manuel Cano <manuelcano406@gmail.com>
2023-08-10 00:45:11 -0300

    Cambios

--- asset/ejemplo1.js
+++ asset/ejemplo1.js
@@ -1,5 +1,66 @@
-let nombre = prompt("Ingrese su nombre y apellido");
- alert("Hola " + nombre);
+const productos = [
+    {nombre: "Taza", precio: 200},
+    {nombre: "Vaso", precio: 350},
+    {nombre: "Cubiertos", precio: 500},
+    {nombre: "Platos", precio: 750},
+    {nombre: "Jarra", precio: 950},
+];
 
-let numero = prompt("Ingrese su altura");
- alert("Su altura es: " + numero);
+let carrito = []
+
+let seleccion = prompt("Hola, ¿Usted quiere añadir al carrito un producto?")
+
+while (seleccion != "si" && seleccion !="no") {
+    alert ("Por favor ingrese su respuesta")
+    seleccion = prompt("Ingrese si o no")
+}
+
+if(seleccion == "si") {
+    alert ("Esta es la lista de productos de nuestro bazar")
+    let listadePrecios = productos.map ((productos) => productos.nombre + " " + productos.precio + "$");
+
+    alert (listadePrecios.join (" - "))
+} else if (seleccion == "no") {
+    alert ("Gracias por visitar nuestra web, esperamos pronto su compra")
+}
+
+while (seleccion != "no") {
+    let producto = prompt ("Seleccioná y agrega un producto")
+    let precio = 0
+
+    if(producto == "Taza" || producto == "Vaso" || producto == "Cubiertos" || producto == "Platos" || producto == "Jarra"){
+        switch (producto) {
+            case "Taza":
+                precio = 200;
+                break;
+            case "Vasos":
+                precio = 350;
+                break;
+            case "Cubiertos":
+                precio = 500;
+                break;
+            case "Platos":
+                precio = 750;
+                break;
+            case "Jarra":
+                precio = 950;
+                break;
+        }
+        
+        let cantidades = parseInt (prompt("¿Cuanta cantidad desea comprar?"))
+        carrito.push ({producto, cantidades, precio})
+    }
+
+    seleccion = prompt("¿Desea seguir añadiendo productos?")
+
+    while(seleccion === "no") {
+        alert("Gracias por su compra")
+        carrito.forEach((carritoLleno) => {
+            console.log (`producto: ${carritoLleno.producto}, cantidades: ${carritoLleno.cantidades}, Total a pagar en su compra ${carritoLleno.cantidades * carritoLleno.precio}`)
+        })
+    break
+    }
+}
+
+const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidades, 0)
+console.log(`Total a pagar por su compra es: ${total}`)
--- asset/ejemplo2.js
+++
@@ -1,9 +0,0 @@
-let edad = parseFloat(prompt("Ingresar su edad"));
-
- if (edad == 18) {
-     alert("Puedes inscribirte al curso")
- }
-
- if (edad < 17) {
-     alert("No puedes inscribirte al curso")
- }
